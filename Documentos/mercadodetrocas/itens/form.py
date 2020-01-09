757005278d4885f1a187a64981ba58fcc07f69af
Jhonan01 <kesse_jonatas85@hotmail.com>
2020-01-09 09:52:51 -0500

    lances feitoo

--- Documentos/mercadodetrocas/itens/form.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Incluir_item
-
-class Incluir_itemForm(ModelForm):
-    class Meta:
-        model = Incluir_item
-
-        fields = ['titulo', 'descricao', 'imagem1', 'imagem2', 'imagem3', 'imagem4', 'imagem5', 'categorias']
-
